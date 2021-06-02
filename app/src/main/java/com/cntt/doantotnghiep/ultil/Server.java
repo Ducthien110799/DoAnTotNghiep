51cc5d0b6ad55a64b492f66e1ed843f48b736d0b
LAPTOP-RMRLH846\nguye <nguyenthien110799@gmail.com>
2021-06-03 02:01:07 +0700

    áda

--- app/src/main/java/com/cntt/doantotnghiep/ultil/Server.java
+++ app/src/main/java/com/cntt/doantotnghiep/ultil/Server.java
@@ -1,10 +1,13 @@
 package com.cntt.doantotnghiep.ultil;
 
 public class Server {
-    public static String localhost ="westsitetitishop.000webhostapp.com";
-    public static String DuongdanLoaisp = "http://" + localhost+ "/server/getloaisp.php";
-    public static String Duongdansanphammoinhat = "http://" + localhost+ "/server/getsanphammoinhat.php";
-    public static String Duongdanaothunphong = "http://" + localhost+ "/server/getsanpham.php?page=";
-    public static String Duongdandonhang = "http://" + localhost+ "/server/thongtinkhachhang.php";
-    public static String Duongdanchitietdonhang = "http://" + localhost+ "/server/chitietdonhang.php";
+    public static String localhost ="ttwestside.000webhostapp.com";
+    public static String DuongdanLoaisp = "http://" + localhost+ "/app/getloaisp.php";
+    public static String Duongdansanphammoinhat = "http://" + localhost+ "/app/getsanphammoinhat.php";
+    public static String Duongdanaothunphong = "http://" + localhost+ "/app/getsanpham.php?page=";
+    public static String Duongdandonhang = "http://" + localhost+ "/app/thongtinkhachhang.php";
+    public static String Duongdanchitietdonhang = "http://" + localhost+ "/app/chitietdonhang.php";
+    public static String URLLogin = "http://"+localhost+"/app/login.php?email=";
+    public static String URLAllsanpham = "http://" + localhost+ "/app/getallsanpham.php";
+    public static String URLRegister = "http://"+localhost+"/app/register.php";
 }
